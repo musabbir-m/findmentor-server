fc388fae10390824f435782eadfbb9f0a598ef02
Musabbir-pr <musabbir313@gmail.com>
2023-03-26 12:31:25 +0600

    feat(index.js): import and use jsonwebtoken library refactor(index.js): move jwt import to the top of the file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
+const jwt= require("jsonwebtoken")
+
 const port = process.env.PORT || 5000;
 // http *****//
 const https = require("https");
